e1c2d95f6d786d5372dc341b200634bcd2977e36
Alvin Xue <19786654@qq.com>
2016-12-05 18:56:09 +0800

    add graph

---
+++ src/graph/graph.py
@@ -0,0 +1,58 @@
+class Graph:
+    """
+    undirected weighted graph
+    """
+
+    def __init__(self, vertex_num=0):
+        """
+        initialize the graph with several vertex
+        :@param vertex_size: number of the initial vertex
+        """
+        self._adj_lists = [[] for i in range(vertex_num)]
+        self._edge_num = 0
+
+    def VertexNum(self):
+        """
+        number of vertices in the graph
+        """
+        return len(self._adj_lists)
+
+    def EdgeSize(self):
+        """
+        number of edges in the graph
+        """
+        return self._edge_num
+
+    def AddEdge(self, vertex1, vertex2, weight):
+        """
+        add a edge between vertex1 and vertex2 with weight
+        :@return: True if success, False if vertex1 or vertex2 is out of range
+        """
+        if vertex1 >= self.VertexNum() or vertex2 >= self.VertexNum():
+            return False
+
+        self._adj_lists[vertex1].append((vertex2, weight))
+        self._adj_lists[vertex2].append((vertex1, weight))
+        self._edge_num += 1
+        return True
+
+    def AddVertex(self):
+        """
+        add an vertex of the next index
+        :@return: the index of the vertex added
+        """
+        self._adj_lists.append(list())
+        return len(self._adj_lists) - 1
+
+    def AdjacentTo(self, vertex):
+        """
+        getter for all vertices adjacent to the vertex and its weight
+        :@param vertex: the index of the vertex
+        :@return: a list of two-tuple, whose first element is the index of the
+                  vertex, second is the weight of the edge between. None if
+                  the index of the vertex is out of range
+        """
+        if vertex >= self.VertexNum():
+            return None
+
+        return self._adj_lists[vertex].copy()
---
+++ src/graph/spfa.py
@@ -0,0 +1,43 @@
+from collection import deque
+
+
+def Spfa(graph, start):
+    if start >= graph.VertexNum():
+        return None
+
+    distance_to = [-1] * graph.VertexNum()
+    vertex_to = [-1] * graph.VertexNum()
+
+    distance_to[start] = 0
+    vertex_to[start] = start
+
+    bfs_q = deque()
+    bfs_q.append(start)
+
+    while bfs_q:
+        cur_vertex = bfs_q.popleft()
+        for vertex, weight in graph.AdjacentTo(cur_vertex):
+            if distance_to[
+                    vertex] == -1 or distance_to[vertex] > distance_to[
+                            cur_vertex] + weight:
+                distance_to[vertex] = weight + distance_to[cur_vertex]
+                vertex_to[vertex] = cur_vertex
+                if cur_vertex not in bfs_q:
+                    bfs_q.append(vertex)
+
+    return _GetSpfaResult(start, distance_to, vertex_to)
+
+
+def _GetSpfaResult(start, distance_to, vertex_to):
+    result = []
+    for vertex, dist in enumerate(distance_to):
+        if dist == -1:
+            result.append((-1, []))
+            continue
+        path = []
+        while vertex != distance_to[vertex_to]:
+            path.append(vertex)
+            vertex = vertex_to[vertex]
+        path.append(start)
+        result.append((dist, path.reverse()))
+    return result
